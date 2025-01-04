41eec4dae3bba2ac159c3390e9f429e86a9405b4
Vipul Kumar <vipul.kumar@taazaa.com>
2025-01-04 19:01:24 +0530

    first commit

---
+++ src/main/java/com/vipuldev/leetcode/problems/LeetcodeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.vipuldev.leetcode.problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeProblemsApplication.class, args);
+	}
+
+}
