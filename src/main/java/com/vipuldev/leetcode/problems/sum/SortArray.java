80d42ba77ac1ea194a20e71c39d4071199560d07
Vipul Kumar <vipul.kumar@taazaa.com>
2025-01-12 22:58:58 +0530

    Sorted array element using brute force method

---
+++ src/main/java/com/vipuldev/leetcode/problems/sum/SortArray.java
@@ -0,0 +1,28 @@
+package com.vipuldev.leetcode.problems.sum;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String []args){
+        int [] unsortedArr = new int[]{ 11,23,22,67,2,5,3,1,56,34};
+        int [] sortedArr = getSortedArr(unsortedArr);
+        System.out.println("Sorted Array: "+ Arrays.toString(sortedArr));
+    }
+
+    public static int [] getSortedArr(int [] unsortedArr){
+        for(int i=0;i<unsortedArr.length;i++){
+            for(int j=i+1;j<unsortedArr.length;j++){
+                if(unsortedArr[i] > unsortedArr[j]){
+                    swapArrayElement(unsortedArr,i,j);
+                }
+            }
+        }
+        return unsortedArr;
+    }
+
+    public static void swapArrayElement(int [] unsortedArr,int i,int j){
+        int temp = unsortedArr[i];
+        unsortedArr[i] = unsortedArr[j];
+        unsortedArr[j] = temp;
+    }
+}
